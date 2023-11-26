d85d7c9e7e4c1b9b12d5a41cbd112e953e22a87c
Georgi Petrov <hailanimepenchou@gmail.com>
2023-11-27 01:47:55 +0200

    Add code to product index table

--- ProductApp/Models/ViewModels/IndexProductViewModel.cs
+++ ProductApp/Models/ViewModels/IndexProductViewModel.cs
@@ -9,4 +9,7 @@ public class IndexProductViewModel
     public Categories Category { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
+
+    public string Code
+        => $"{this.Name.Substring(0, 2)}{this.Category.ToString().Substring(0, 3)}{this.Id.ToString().Substring(0, 3)}";
 }

73f4acd311562771c88c238a2cb356d01c46cc62
Georgi Petrov <hailanimepenchou@gmail.com>
2023-11-26 22:09:24 +0200

    Add Reliability ConfigureAwait supression

--- CompanyManagerApp/GlobalSuppressions.cs
+++ CompanyManagerApp/GlobalSuppressions.cs
@@ -1,4 +1,4 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0073:The file header is missing or not located at the top of the file", Justification = "<Pending>")]
+[assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:CompanyManagerApp.Controllers")]
